62a2025925703e7102da4c16e02b5519e1366612
Omar Issa <omarissa@my.unt.edu>
2022-11-29 23:18:30 -0600

    bad request header sent instead of failure

--- api/routes/createuser.js
+++ api/routes/createuser.js
@@ -26,7 +26,7 @@ router.get("/", async function (req, res, next) {
 	var otp = Math.floor(Math.random() * 10 ** 6);
 
 	if (isPhoneSanitized(req.headers["x-korrero-phone"]) || isEmailSanitized(req.headers["x-korrero-email"]) /*|| doesUserExist*/ || carrierDomains[req.headers["x-korrero-carrier"]]) {
-		res.send("Failure");
+		res.send(400);
 		return;
 	}
 
