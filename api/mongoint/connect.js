0f50ea6134783feea731f6c454317a700b1bf477
Omar Issa <omarissa@my.unt.edu>
2023-04-15 23:32:47 -0500

    env changes

--- api/mongoint/connect.js
+++ api/mongoint/connect.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 require('dotenv').config();
 
-const uri = `mongodb://${process.env.MONGOUSER}:${encodeURIComponent(process.env.MONGOPASSWORD)}@192.252.223.33:27017/?authMechanism=DEFAULT`;
+const uri = `mongodb://${process.env.MONGOUSER}:${encodeURIComponent(process.env.MONGOPASSWORD)}@${process.env.MONGOIP}:27017/?authMechanism=DEFAULT`;
 const client = new MongoClient(uri);
 
 module.exports = client;
