dd3486864bcf950dabf4316e9b2d8c814bcb3b0a
Theriz0 <skandha2k@gmail.com>
2023-04-17 00:15:47 +0000

    hotfix testlive.js

--- api/test/testlive.js
+++ api/test/testlive.js
@@ -21,24 +21,24 @@ describe('Live Route', function() {
       done();
     };
     router.handle({ method: 'get', url: '/' }, res, next);
-
-   //Uncomment this to test case any error codes here
-   //it('should return a 404 status code', function(done) { // change expected status code to 404
-   //     const res = {
-   //       status: function(code) {
-   //       this.statusCode = code;
-   //         return this;
-   //       },
-   //       send: function() {
-   //         assert.strictEqual(res.statusCode, 404, 'Wrong status code returned'); // assert for 404 status code
-   //         done();
-   //       }
-   //     };
-   //     const next = function() {
-   //       // handle the request
-   //       done(new Error('Route should not call next()')); // force the test to fail if next() is called
-   //     };
-   //     router.handle({ method: 'get', url: '/' }, res, next);
-   //   });
     });
+
+    //Uncomment this to test case any error codes here
+   it('should return a 404 status code', function(done) { // change expected status code to 404
+        const res = {
+          status: function(code) {
+          this.statusCode = code;
+            return this;
+          },
+          send: function() {
+            assert.strictEqual(res.statusCode, 404, 'Wrong status code returned'); // assert for 404 status code
+            done();
+          }
+        };
+       const next = function() {
+          // handle the request
+          done(new Error('Route should not call next()')); // force the test to fail if next() is called
+        };
+        router.handle({ method: 'get', url: '/' }, res, next);
+      });
 });
